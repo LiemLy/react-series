591d45299f9b53b9e8a0b29bb6ebdd52762060fd
LL <liembsbs123@gmail.com>
2021-04-22 22:48:15 +0700

    add layout base

--- react-tailwind/src/App.tsx
+++ react-tailwind/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex">
+        <Sidebar />
+        <div className="h-screen w-full pt-8 px-4">
+          <div className="h-full overflow-auto z-0">
+            <div className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              accusantium. Temporibus consectetur eveniet cupiditate obcaecati
+              quia mollitia eligendi dolorum rerum officia qui quas
+              reprehenderit distinctio ad adipisci, ipsum perferendis est!
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
