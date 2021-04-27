da606765ae804366ab4af130a775c0584d157cd8
LL <liembsbs123@gmail.com>
2021-04-27 11:34:41 +0700

    change app purpose

---
+++ 02-fan-wiki/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-1/2 m-auto">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-fan-wiki/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="mt-16">
+      <div>
+        <div>
+          <h1 className="text-5xl font-bold text-center">Pokedex</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
