7bdba3db992804586606b233631773c3c0555347
LL <liembsbs123@gmail.com>
2021-05-09 12:39:57 +0700

    init react app with p5

---
+++ react-p5-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Circle from "./components/Circle";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>P5.JS App</h2>
+
+      <Circle />
+    </div>
+  );
+}
+
+export default App;
